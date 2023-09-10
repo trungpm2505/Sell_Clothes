01d97c2008e6d245ad24735066a0db174e3de346
trungpm <mjnhtrung2003@gmail.com>
2023-09-10 16:53:15 +0700

    no message

--- Sell_Clothes/src/main/java/com/web/SellClothes/controller/LoginController.java
+++ Sell_Clothes/src/main/java/com/web/SellClothes/controller/LoginController.java
@@ -1,16 +1,17 @@
-package com.example.demo;
+package com.web.SellClothes.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-
-public class DemoController {
+@RequestMapping("/login")
+public class LoginController {
 	
 	@ResponseBody
 	@GetMapping
-	public String demo() {
-		return "Đây là dự án tốt nghiệp";
+	public String login() {
+		return"Đây là trang login!";
 	}
 }
