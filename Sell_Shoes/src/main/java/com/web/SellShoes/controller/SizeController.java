9c6fc3544a33ce62a29040493e168542ad2a026a
Cong an <pnca@gmail.com>
2023-09-20 16:23:39 +0700

    size 1

--- Sell_Shoes/src/main/java/com/web/SellShoes/controller/SizeController.java
+++ Sell_Shoes/src/main/java/com/web/SellShoes/controller/SizeController.java
@@ -1,9 +1,12 @@
 package com.web.SellShoes.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SizeController {
+	@GetMapping("view")
+	
 	public String view() {
 		return"";
 	}
