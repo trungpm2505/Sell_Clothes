eada9244bfe87c3d2f1d3cee2e8751655a700373
QuocNdowe <dung45131@gmail.com>
2023-09-20 17:21:09 +0700

    test code

--- Sell_Shoes/src/main/java/com/web/SellShoes/controller/SizeController.java
+++ Sell_Shoes/src/main/java/com/web/SellShoes/controller/SizeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SizeController {
-	@GetMapping("ODer")
+	@GetMapping("ODer123")
 	
 	public String view() {
 		return"/view";
